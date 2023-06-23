a33cfb22f57c8464d07cfe94ac53b1eab4ef85bc
Leslie Zhou <ohlesl1e420@gmail.com>
2023-06-23 13:54:04 -0700

    Delete application/sveltekit directory

--- application/sveltekit/src/lib/config.js
+++
@@ -1,8 +0,0 @@
-export const NETWORKS = [
-    {
-        chainId: 5777,
-        rpcUrl: 'http://127.0.0.1:7545',
-        chainName: 'Custome Network',
-        nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 }
-    }
-]
